f18f9818232d571803d6ee32ffda188017cd18a5
santttii <santiboquense118@gmail.com>
2024-05-28 12:55:00 -0300

    JS agregado con funcion de verificacion

---
+++ main.js
@@ -0,0 +1,16 @@
+function validarCampos() {
+    const email = document.getElementById('mail').value;
+    const contrasena = document.getElementById('pass').value;
+
+    if (email && contrasena) {
+        console.log('Campos completados');
+
+    } else {
+        console.log('Campos vacíos');
+        Swal.fire({
+            icon: 'error',
+            title: 'Error...',
+            text: 'Por favor, completa ambos campos para continuar.'
+        })
+    }
+}
